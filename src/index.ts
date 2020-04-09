e9494cec02f08e0033b343cdc681bc9318394f7c
Jon Guy <jon.guy@live.com>
2020-04-09 17:02:46 -0400

    queue changes inside of createState, protect against undefined values when using select

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Observable, of, isObservable, combineLatest, BehaviorSubject } from 'rxjs';
-import { map, distinctUntilChanged, switchMap, reduce } from 'rxjs/operators'
+import { map, distinctUntilChanged, switchMap, reduce, filter } from 'rxjs/operators'
 import { html as litHtml, TemplateResult } from 'lit-html';
 
 function makeObservable<T>(arg: T): Observable<unknown> {
@@ -42,22 +42,26 @@ export type State<T> = Observable<T> & Reducable<T> & Selectable<T>
 
 export function createState<T>(initialValue: T, reduceHandler?: ReduceHandler<T>) {
   const subject = new BehaviorSubject(initialValue);
+  let pendingChanges = Promise.resolve(initialValue);
 
   const reducable = Object.assign(subject, {
     reduce: (reducer: Reducer<T>) => {
-      const nextValueOrPromise = reduceHandler
-        ? reduceHandler(subject.value, reducer)
-        : reducer(subject.value);
-
-      return Promise.resolve(nextValueOrPromise)
-        .then(nextValue => {
-          if (nextValue === subject.value) {
-            return subject.value;
+      pendingChanges = Promise.resolve(pendingChanges)
+        .then(current => {
+          if (reduceHandler) {
+            return reduceHandler(current, reducer);
           } else {
-            subject.next(nextValue);
-            return nextValue;
+            return Promise.resolve(reducer(current)).then(next => {
+              if (subject.value !== next) {
+                subject.next(next);
+              }
+
+              return next;
+            })
           }
         });
+
+      return pendingChanges;
     }
   });
 
@@ -70,12 +74,17 @@ export function createState<T>(initialValue: T, reduceHandler?: ReduceHandler<T>
 
 export function select<T, TKey extends keyof T>(source: Observable<T> & Reducable<T>, key: TKey): State<T[TKey]> {
   const observable = source.pipe(
+    filter(val => val !== undefined),
     map(val => val[key])
   );
 
   const reducable = Object.assign(observable, {
     reduce: (reducer: Reducer<T[TKey]>) => 
       source.reduce(current => {
+        if (current === undefined) {
+          return current;
+        }
+
         const next = reducer(current[key]);
 
         return Promise.resolve(next).then(nextValue => {
@@ -100,6 +109,7 @@ export function select<T, TKey extends keyof T>(source: Observable<T> & Reducabl
 
 export function mapItems<T, U>(itemsState: State<T[]>, mapping: (itemState: State<T>, index: number) => U): Observable<Observable<U>[]> {
   const out = itemsState.pipe(
+    filter(items => items !== undefined),
     switchMap(items => {
       const itemStates = items.map((item, index) => 
         createState(item, (current, reducer) =>
